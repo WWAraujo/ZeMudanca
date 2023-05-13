a8c6303d540ae2902d5b8c1d160330a8285fd55e
wallace.waraujo <wallace20130@gmail.com>
2023-05-12 22:16:33 -0300

    novas att

--- clientes/src/main/java/br/com/zemudanca/dao/LoginDAO.java
+++ clientes/src/main/java/br/com/zemudanca/dao/LoginDAO.java
@@ -1,10 +1,41 @@
 package br.com.zemudanca.dao;
 
+import br.com.zemudanca.model.Login;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
 public class LoginDAO {
 
-    public void altorizarLogin() {
+    public Login altorizarLogin(Login login) {
 
-        String SQL = "SELECT UserId FROM LOGIN WHERE USERNAME = 'admin' and password = 'admin' ;";
+        String SQL = "SELECT UserId FROM LOGIN WHERE USERNAME = '"
+                + login.getUserName()+"' and password = '"+login.getUserPassword()+"' ;";
 
+        boolean resposta = false;
+
+        try {
+            Connection connection = DriverManager.getConnection("jdbc:h2:~/test", "sa", "sa");
+
+            System.out.println("success in database connection");
+
+            PreparedStatement preparedStatement = connection.prepareStatement(SQL);
+
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            if (resultSet != null){
+                login.setUserId(resultSet.getString("UserId"));
+                login.setVerificado(1);
+            }
+
+            return login;
+
+        } catch (Exception e){
+
+            System.out.println("Acesso negado!");
+            return login;
+        }
     }
 }
--- clientes/src/main/java/br/com/zemudanca/model/Login.java
+++ clientes/src/main/java/br/com/zemudanca/model/Login.java
@@ -2,5 +2,56 @@ package br.com.zemudanca.model;
 
 public class Login {
 
-    private String userName, userId;
+    private String userName, userId, userPassword;
+
+    /**
+     *  0 - false
+     *  1 true
+     */
+
+    int verificado;
+
+    public Login(String userName, String userId) {
+        this.userName = userName;
+        this.userId = userId;
+    }
+
+    public Login(String userName, String userId, String userPassword, int verificado) {
+        this.userName = userName;
+        this.userId = userId;
+        this.userPassword = userPassword;
+        this.verificado = verificado;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getUserPassword() {
+        return userPassword;
+    }
+
+    public void setUserPassword(String userPassword) {
+        this.userPassword = userPassword;
+    }
+
+    public int getVerificado() {
+        return verificado;
+    }
+
+    public void setVerificado(int verificado) {
+        this.verificado = verificado;
+    }
 }
